b7e5eb5b68575c88b7ecb9e2204c5e3906ac8998
Narelsiak <michalekxddlol@gmail.com>
2025-03-26 20:49:38 +0000

    Base MLP model

---
+++ ai/data/dataset.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import numpy as np
+import os
+
+def load_data(file_path):
+    """Loads data from a CSV file and returns X (features) and y (one-hot encoded labels)."""
+    try:
+        df = pd.read_csv(file_path)
+    except FileNotFoundError:
+        raise FileNotFoundError(f"File not found: {file_path}")
+    except Exception as e:
+        raise Exception(f"Error while loading the file: {e}")
+    
+    X = df.drop(columns=['Id', 'Species'])
+    y = one_hot_encoder(df['Species'])
+    return X, y
+
+def one_hot_encoder(labels):
+    """Converts labels to one-hot encoding."""
+    unique_labels = np.unique(labels)
+    label_to_index = {label: index for index, label in enumerate(unique_labels)}
+    label_indices = np.array([label_to_index[label] for label in labels])
+    return np.eye(len(unique_labels))[label_indices]
+
+def split_dataset(X, y, test_size=0.2):
+    """Splits dataset into train and test sets, ensuring balanced class distribution."""
+    num_samples = len(y)
+    indices = np.arange(num_samples)
+    np.random.shuffle(indices)
+
+    split_idx = int(num_samples * (1 - test_size))
+    train_indices, test_indices = indices[:split_idx], indices[split_idx:]
+
+    X_train, X_test = X.iloc[train_indices].values, X.iloc[test_indices].values
+    y_train, y_test = y[train_indices], y[test_indices]
+
+    return X_train, X_test, y_train, y_test
+
+def data_normalization(X):
+    """Normalizes the dataset using Min-Max scaling."""
+    return (X - X.min()) / (X.max() - X.min())
+
+file_path = os.path.join('ai_analysis', 'data', 'raw', 'Iris.csv')
+
+X, y = load_data(file_path)
+X = data_normalization(X)
+X_train, X_test, y_train, y_test = split_dataset(X, y)
---
+++ ai/models/tensorflow_seq.py
@@ -0,0 +1,22 @@
+import tensorflow as tf
+from data.dataset import X_train, y_train, X_test, y_test
+
+
+model = tf.keras.Sequential([  
+    # First hidden layer with 10 neurons, ReLU activation, and input shape of (4,) (4 features)
+    tf.keras.layers.Dense(10, input_shape=(4,), activation='relu'),
+    
+    # Second hidden layer with 8 neurons and ReLU activation
+    tf.keras.layers.Dense(8, activation='relu'),
+    
+    # Output layer with 3 neurons (for 3 classes) and softmax activation for classification
+    tf.keras.layers.Dense(3, activation='softmax')
+])
+
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+model.fit(X_train, y_train, epochs=230, verbose=1)
+
+loss, accuracy = model.evaluate(X_test, y_test, verbose=0)
+
+print(f'Accuracy: {accuracy:.4f}')
