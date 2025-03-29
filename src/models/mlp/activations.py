1b2832a4fd09740e2a454e7180c1eff20b7bed7d
Narelsiak <michalekxddlol@gmail.com>
2025-03-29 22:10:53 +0000

    -activations function

---
+++ src/models/mlp/activations.py
@@ -0,0 +1,39 @@
+import numpy as np
+
+def relu(x):
+    """ReLU activation function."""
+    return np.maximum(0, x)
+
+def relu_derivative(x):
+    """Derivative of ReLU activation function."""
+    return np.where(x > 0, 1, 0)
+
+def leaky_relu(x, alpha=0.01):
+    """Leaky ReLU activation function."""
+    return np.where(x > 0, x, alpha * x)
+
+def sigmoid(x):
+    """Sigmoid activation function."""
+    return 1 / (1 + np.exp(-x))
+
+def sigmoid_derivative(x): 
+    """Derivative of Sigmoid activation function."""
+    return x * (1 - x)
+
+def tanh(x):
+    """Tanh activation function."""
+    return np.tanh(x)
+
+def tanh_derivative(x):
+    """Derivative of Tanh activation function."""
+    return 1 - np.tanh(x)**2
+
+def softmax(x):
+    """Softmax activation function."""
+    e_x = np.exp(x - np.max(x))
+    return e_x / e_x.sum(axis=0)
+
+def softmax_derivative(x):  
+    """Derivative of Softmax activation function."""
+    s = softmax(x)
+    return np.diag(s) - np.outer(s, s)
