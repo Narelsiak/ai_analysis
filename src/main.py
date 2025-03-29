dbf2f5653993c1414106441dd421aa60466c027d
Narelsiak <michalekxddlol@gmail.com>
2025-03-29 09:33:06 +0000

    -new model

--- src/main.py
+++ src/main.py
@@ -8,6 +8,7 @@ from src.models.MLP_L2 import train as train_MLP_L2
 from src.models.MLP_ADAM import train as train_MLP_ADAM
 from src.models.MLP_ADAM_L2 import train as train_MLP_ADAM_L2
 from src.models.MLP_tf import train as train_MLP_tf
+from src.models.MLP_ADAM_batch import train as train_MLP_ADAM_batch
 
 from src.visualization.explore_data import analyze_data
 from src.visualization.analyze_training_results import analyze_training_results
@@ -20,11 +21,13 @@ def main():
         "MLP Base": train_MLP_base,
         "MLP L2": train_MLP_L2,
         "MLP Adam": train_MLP_ADAM,
-        "MLP Adam + L2": train_MLP_ADAM_L2
+        "MLP Adam + L2": train_MLP_ADAM_L2,
+        "MLP Adam Batch": train_MLP_ADAM_batch,
+        "MLP Tensorflow": train_MLP_tf
     }
 
     for model_name, model_func in models.items():
-        loss_history, accuracy_history, y_test_classes, predictions, accuracy = model_func(X_train, y_train, X_test, y_test)
+        loss_history, accuracy_history, y_test_classes, predictions, accuracy = model_func(X_train, y_train, X_test, y_test, learning_rate=0.01)
         results[model_name] = {
             "loss": loss_history,
             "accuracy": accuracy_history,
